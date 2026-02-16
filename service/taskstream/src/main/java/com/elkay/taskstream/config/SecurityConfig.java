c50bb2f5bd427b9517436c6c207fbae765d82074
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2026-02-17 00:30:09 +0530

    updated swagger routes to security config

--- service/taskstream/src/main/java/com/elkay/taskstream/config/SecurityConfig.java
+++ service/taskstream/src/main/java/com/elkay/taskstream/config/SecurityConfig.java
@@ -56,6 +56,9 @@ public class SecurityConfig {
                                 "/api/v1/auth/signup",
                                 "/api/v1/auth/send_verification_code",
                                 "/api/v1/auth/reset_password",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
                                 "/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // allow preflight for all endpoints
                         .anyRequest().authenticated()
